ac6794f237a08555c624f561f1e8b7f41bd7b4d7
Nimsilu <101490767+Nimsilu@users.noreply.github.com>
2022-04-16 22:21:02 +0530

    Update game_api.py

--- game_api.py
+++ game_api.py
@@ -4,44 +4,37 @@ from threading import Thread
 
 from api import API
 from chatter import Chat_Message, Chatter
+from enums import Game_Status
+from lichess_game import Lichess_Game
 
-using_me = False
-
-if using_me:
-    from lichess_game2 import Lichess_Game
-else:
-    from lichess_game import Lichess_Game
 
 class Game_api:
-    def __init__(self, config: dict, api: API, username: str, game_id: str):
+    def __init__(self, config: dict, api: API, game_id: str) -> None:
         self.config: dict = config
         self.api = api
-        self.username = username
         self.game_id = game_id
         self.chatter = Chatter(config)
         self.ping_counter = 0
         self.game_queue = Queue()
+        self.was_aborted = False
 
-    def run_game(self):
+    def run_game(self) -> None:
         game_queue_thread = Thread(target=self._watch_game_stream, daemon=True)
         game_queue_thread.start()
-       
-        self.api.send_chat_message(self.game_id, "player", "Hello! I am a Bot")
+
+        self.api.send_chat_message(self.game_id, "player", "Hello!I am bot")
         self.api.send_chat_message(self.game_id, "player", "I wish you good luck!")
 
         self.api.send_chat_message(self.game_id, "spectator", "Welcome friends!")
         self.api.send_chat_message(self.game_id, "spectator", "Thanks for watching my games!")
 
-
+        
         while True:
             event = self.game_queue.get()
 
-            if event['type'] == 'aborted':
-                print(f'Game "{self.game_id}" was aborted.')
-                break
-            elif event['type'] == 'gameFull':
+            if event['type'] == 'gameFull':
                 print(f'Game "{self.game_id}" was started.')
-                self.lichess_game = Lichess_Game(self.api, event, self.config, self.username)
+                self.lichess_game = Lichess_Game(self.api, event, self.config)
 
                 if self.lichess_game.is_our_turn():
                     uci_move, offer_draw, resign = self.lichess_game.make_move()
@@ -52,8 +45,13 @@ class Game_api:
             elif event['type'] == 'gameState':
                 updated = self.lichess_game.update(event)
 
-                if event['status'] != 'started' or self.lichess_game.is_game_over():
+                if self.lichess_game.status != Game_Status.STARTED:
+                    self.was_aborted = self.lichess_game.status == Game_Status.ABORTED
+                    print(self.lichess_game.get_result_message(event.get('winner')))
                     break
+
+                if self.lichess_game.is_game_over():
+                    continue
 
                 if self.lichess_game.is_our_turn() and updated:
                     uci_move, offer_draw, resign = self.lichess_game.make_move()
@@ -63,13 +61,19 @@ class Game_api:
                         self.api.send_move(self.game_id, uci_move, offer_draw)
             elif event['type'] == 'chatLine':
                 chat_message = Chat_Message(event)
+
+                if chat_message.username == 'lichess':
+                    if chat_message.room == 'player':
+                        print(f'{chat_message.username}: {chat_message.text}')
+                    continue
+
                 print(f'{chat_message.username} ({chat_message.room}): {chat_message.text}')
 
                 if chat_message.text.startswith('!'):
-                     command = chat_message.text[1:].lower()
-                     response = self.chatter.react(command, self.lichess_game)
+                    command = chat_message.text[1:].lower()
+                    response = self.chatter.react(command, self.lichess_game)
 
-                     self.api.send_chat_message(self.game_id, chat_message.room, response)
+                    self.api.send_chat_message(self.game_id, chat_message.room, response)
             elif event['type'] == 'ping':
                 self.ping_counter += 1
 
@@ -77,16 +81,10 @@ class Game_api:
                     self.api.abort_game(self.game_id)
             else:
                 print(event)
-        
-        self.api.send_chat_message(self.game_id, "spectator", "Bye!")
-        self.api.send_chat_message(self.game_id, "player", "Hey, GG! Thanks for game!")
-        
-        print(f'Game {self.game_id} over')
-
 
         self.lichess_game.quit_engine()
 
-    def _watch_game_stream(self):
+    def _watch_game_stream(self) -> None:
         game_stream = self.api.get_game_stream(self.game_id)
 
         for line in game_stream:
